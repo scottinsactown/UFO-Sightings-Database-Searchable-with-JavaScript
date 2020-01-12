38f06b6cffbe51fd3d17af4900d17b00364bed78
Scott Clark <scottinsacto@yahoo.com>
2020-01-11 18:20:10 -0800

    table fill

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
-var tableData = data;
+let tableData = data;
+let tableHTML = d3.select("tbody");
+console.log(tableHTML)
 
-// YOUR CODE HERE!
+tableData.forEach(function(ufoReport) {
+    // console.log(ufoReport);
+    let row = tableHTML.append("tr");
+    Object.entries(ufoReport).forEach(function([key, value]) {
+    //   console.log(key, value);
+      // Append a cell to the row for each value
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
