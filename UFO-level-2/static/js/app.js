83747f2e9971d554ca42d1a57405f9efe0f930ea
Scott Clark <scottinsacto@yahoo.com>
2020-01-12 11:30:50 -0800

    working on filter array loop

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,55 +1,81 @@
 // from data.js
 let tableData = data;
 let tableHTML = d3.select("tbody");
-console.log(tableHTML)
+let dateData = d3.select("#datetime");
+let cityData = d3.select("#city");
+let stateData = d3.select("#state");
+console.log(tableHTML);
 
 tableData.forEach(function(ufoReport) {
     // console.log(ufoReport);
     let row = tableHTML.append("tr");
     Object.entries(ufoReport).forEach(function([key, value]) {
     //   console.log(key, value);
-      // Append a cell to the row for each value
       let cell = row.append("td");
       cell.text(value);
     });
   });
-
-//   * Use a date form in your HTML document and write JavaScript code
-//    that will listen for events and search through the `date/time` column
-//    to find rows that match user input.
 
 // Select the button
 let button = d3.select("#filter-btn");
 
 button.on("click", function() {
-  // Reset table
+  // Reset table on webpage
   let list = d3.select("tbody");
   list.html("");
+  // Reset table to filter
+  let filteredData = tableData;
+  let filterArray = []
+  
+  let filterDate = dateData.property("value");
+  console.log(filterDate);
+  filterArray.push(filterDate);
+ 
+  let filterCity = cityData.property("value").trim().toLowerCase();
+  console.log(filterCity);
+  filterArray.push(filterCity);
 
-  // Select the input element and get the raw HTML node
-  let inputElement = d3.select("#datetime");
-  let inputElementCity = d3.select("#city");
+  let filterState = stateData.property("value").trim().toLowerCase();
+  console.log(filterState);
+  filterArray.push(filterState);
 
-  // Get the value property of the input element
-  let inputValue = inputElement.property("value");
-  let inputValueCity = inputElementCity.property("value");
+  console.log(filterArray);
 
-//   console.log(inputValue);
+  if (filterDate !== "") {
+    filteredData = filteredData.filter(data => data.datetime === filterDate);
+  }
+  else {filteredData};
 
-  let filteredData = tableData.filter(date => date.datetime === inputValue);
-  let filteredData2 = filteredData.filter(city =>city.city === inputValueCity);
+  if (filterCity !== "") {
+    filteredData = filteredData.filter( data => data.city === filterCity);
+  }
+  else {filteredData};
 
+  if (filterState !== "") {
+    filteredData = filteredData.filter( data => data.state === filterState);
+  }
+  else {filteredData};
+
+// filterArray.forEach(function(userInput) {
+//   let inputElements = d3.select(userInput);
+//   console.log(inputElements);
+//   let inputValues = inputElements.property("value");
+//   console.log(inputValues);
+
+//   // if inputValues !== null;
+//   filteredData = tableData.filter(item => item.***userInput*** === inputValues);
 //   console.log(filteredData);
+// });
 
-  filteredData2.forEach(function(ufoReport) {
+  filteredData.forEach(function(ufoReport) {
     // console.log(ufoReport);
     let row = tableHTML.append("tr");
     Object.entries(ufoReport).forEach(function([key, value]) {
     //   console.log(key, value);
-      // Append a cell to the row for each value
       let cell = row.append("td");
       cell.text(value);
     });
   });
 
+// })
 });
