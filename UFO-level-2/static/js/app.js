cb8a777ecf93b60c26745facc706feff321e9a99
Scott Clark <scottinsacto@yahoo.com>
2020-01-13 18:33:55 -0800

    enter click

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,3 +1,6 @@
+console.log('enhancements made: take different date formats, ignore uppercase for other fields'
+)
+
 // from data.js
 let tableData = data;
 let tableHTML = d3.select("tbody");
@@ -9,6 +12,21 @@ let stateData = d3.select("#state");
 let countryData = d3.select("#country");
 let shapeData = d3.select("#shape");
 // console.log(tableHTML);
+
+// window.onload = function(){
+//   const distinctShapes = [...new Set(data.map(x => x.shape))];
+//   document.getElementById('demo').innerHTML = distinctShapes;
+// }
+
+// let text = "<ul>";
+// distinctShapes.forEach(myFunction);
+// text += "</ul>";
+// document.getElementById("demo").innerHTML = text;
+
+// function myFunction(value) {
+//   text += "<li>" + value + "</li>";
+// } 
+
 
 tableData.forEach(function(ufoReport) {
     // console.log(ufoReport);
@@ -23,37 +41,38 @@ tableData.forEach(function(ufoReport) {
 // Select the button
 let button = d3.select("#filter-btn");
 
-button.on("click", function() {
+// // // Get the input field or if multiple fields, get the ul id
+let input = document.getElementById("filters")
+// // Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the filter button element with a click
+    document.getElementById("filter-btn").click();
+  }
+});
+
+button.on("click", function submitFilter() {
   // Reset table on webpage
   let list = d3.select("tbody");
   list.html("");
-  // Reset table to filter
+
+  // Reset table to be filtered
   let filteredData = tableData;
-  
-  // Remove all filterArray elements if not using
-  // let filterArray = []
-  
-  let filterDate = dateData.property("value");
+    
+  // \b represents the border between a word character and a non-word character. 
+  // \b0 will match a leading zero, g is global
+  // which is removed so search works with all date formats entered
+  let filterDate = dateData.property("value").replace(/\b0/g, '');
   console.log(filterDate);
-//   filterArray.push({
-//     key:   "datetime",
-//     value: filterDate
-// });
  
   let filterCity = cityData.property("value").trim().toLowerCase();
   console.log(filterCity);
-//   filterArray.push({
-//     key:   "city",
-//     value: filterCity
-// });
 
   let filterState = stateData.property("value").trim().toLowerCase();
   console.log(filterState);
-//   filterArray.push({
-//     key:   "state",
-//     value: filterState
-// });
-  // console.log(filterArray);
 
   let filterCountry = countryData.property("value").trim().toLowerCase();
   console.log(filterCountry);
@@ -67,24 +86,28 @@ button.on("click", function() {
   else {filteredData};
 
   if (filterCity !== "") {
-    filteredData = filteredData.filter( data => data.city === filterCity);
+    filteredData = filteredData.filter(data => data.city === filterCity);
   }
   else {filteredData};
 
   if (filterState !== "") {
-    filteredData = filteredData.filter( data => data.state === filterState);
+    filteredData = filteredData.filter(data => data.state === filterState);
   }
   else {filteredData};
 
   if (filterCountry !== "") {
-    filteredData = filteredData.filter( data => data.country === filterCountry);
+    filteredData = filteredData.filter(data => data.country === filterCountry);
   }
   else {filteredData};
 
   if (filterShape !== "") {
-    filteredData = filteredData.filter( data => data.shape === filterShape);
+    filteredData = filteredData.filter(data => data.shape === filterShape);
   }
   else {filteredData};
+
+  // if (filteredData !== []) {
+  // console.log(distinctShapes);
+  // };
 
 // need to assign to key/value pattern
 // filterArray.forEach(function(userInput) {
@@ -102,6 +125,14 @@ button.on("click", function() {
 //   filteredData = tableData.filter(item => item.***userInput*** === inputValues);
 //   console.log(filteredData);
 // });
+
+
+//   filterArray.push({
+//     key:   "state",
+//     value: filterState
+// });
+  // console.log(filterArray);
+
 
   filteredData.forEach(function(ufoReport) {
     // console.log(ufoReport);
